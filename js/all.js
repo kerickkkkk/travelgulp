528c75de789a8b4fc439076d531a92b59f99744b
eric <howcrazys@gmail.com>
2019-01-30 20:22:50 +0800

    Update 2019-01-30T12:22:43.203Z

---
+++ js/all.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var test = function test() {
+  console.log('test2');
+};
+"use strict";
+
+var test = function test() {
+  console.log('test');
+};
+//# sourceMappingURL=all.js.map
